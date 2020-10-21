e7e887a42121f55a972eb895d81c9c52b783a5f5
Vinit Suryarao <60574327+VinitSuryarao@users.noreply.github.com>
2020-10-21 20:54:17 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _03_Boxing_Unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Boxing :- Convert Value Type to Object Type
+            int a = 102;
+            Object b = a;
+
+            // Unboxing :- Convert Object Type to Value Type
+            object x = 105;
+            int y = Convert.ToInt32(x);
+
+        }
+    }
+}
